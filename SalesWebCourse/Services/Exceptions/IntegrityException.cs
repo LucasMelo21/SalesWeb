c90ddb628150e9aa21eb4291a918bcd6e21b286e
Lucas Melo <lucasmelo.tec.saude@gmail.com>
2025-09-15 21:17:28 -0300

    Exception handling for delete(referential integrity)

---
+++ SalesWebCourse/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesWebCourse.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message) { }
+    }
+}
